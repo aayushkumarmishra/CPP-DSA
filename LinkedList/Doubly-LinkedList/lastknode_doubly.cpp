ad2e3bce15b78e962fd929ad31e3945ddd9ec63a
Ayush Kumar Mishra <ayushmishra9615@gmail.com>
2021-06-14 21:21:18 +0530

    append last k nodes

---
+++ LinkedList/Doubly-LinkedList/lastknode_doubly.cpp
@@ -0,0 +1,79 @@
+// this is append last K Nodes problem of a Linked List, k=3
+
+#include <iostream>
+
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node* &head,int data){
+    node* temp = new node(data);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtTail(node* &head,int data){
+    node* temp1 = new node(data);
+    if(head == NULL){
+        head = temp1;
+        return;
+    }
+    node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = temp1;
+}
+
+void display(node* head){
+    node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+node* lastnodes(node* &head,int k){
+    int length =0;
+    node* temp = head;
+    while(temp->next != NULL){
+        temp = temp ->next;
+        length++;
+    }
+    node* last = temp;
+    k = k%length;
+    int pos = length - k;
+    int count = 0;
+    temp = head;
+    while(temp != NULL && count < pos){
+        temp = temp ->next;
+        count++;
+    }
+    node* lastNode = temp;
+    node* startNode = temp ->next;
+    last ->next = head;
+    lastNode -> next = NULL;
+    return startNode;
+}
+
+int main(){
+
+    node* head = NULL;
+    int arr[] = {1,2,3,4,5,6};
+    for(int i = 0; i< *(&arr + 1) - arr; i++){
+        insertAtTail(head,arr[i]);
+    }
+    display(head);
+    node* updated = lastnodes(head,3);
+    display(updated);
+    return 0;
+}
